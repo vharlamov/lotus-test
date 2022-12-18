916ad95971ccb0d50a754a119dba5ea335c39c3e
Valeriy Harlamov <valera545@gmail.com>
2022-12-18 14:47:39 +0300

    server refactored

--- server/index.js
+++ server/index.js
@@ -4,19 +4,37 @@ import cors from 'cors'
 import chalk from 'chalk'
 import { time, startTime } from './utils/timeGen.js'
 import path from 'path'
+import config from 'config'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
-
-const PORT = 8040
 
 const app = express()
 
 app.use(cors())
 
-app.get('/time', (req, res) => {
-	res.send(time.toString())
-})
+const PORT = config.get('port') ?? 8040
+
+if (process.env.NODE_ENV === 'production') {
+	app.use('/', express.static(path.join(__dirname, 'client')))
+
+	const indexPath = path.join(__dirname, 'client', 'index.html')
+
+	app.get('/', (req, res) => {
+		res.sendFile(indexPath)
+	})
+
+	app.get('/time', (req, res) => {
+		res.send(time.toString())
+	})
+} else {
+	app.get('/time', (req, res) => {
+		res.setHeader('Access-Control-Allow-Method', 'GET').send(time.toString())
+	})
+}
+// app.get('/time', (req, res) => {
+// 	res.send(time.toString())
+// })
 
 app.listen(PORT, () => {
 	console.log(
