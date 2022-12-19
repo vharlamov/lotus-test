20bdbbfdf60579fcd0280cd21e1d890ca52b83f3
Valeriy Harlamov <valera545@gmail.com>
2022-12-19 12:26:41 +0300

    req cors  headers fixed

--- client/src/config.js
+++ client/src/config.js
@@ -1,7 +1,7 @@
 const config = {
 	local: 'http://localhost:8040/',
 	vercel: 'https://lotus-test-server.vercel.app/',
-	fozzy: 'http://23.111.202.40/',
+	fozzy: 'http://23.111.202.40:83/',
 }
 
 export default config.fozzy
