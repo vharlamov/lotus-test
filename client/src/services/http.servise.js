c7ff54cc6324e3943ae14ad45074ca7e481f8374
Valeriy Harlamov <valera545@gmail.com>
2022-12-19 11:03:14 +0300

    req cors  headers fixed

--- client/src/services/http.servise.js
+++ client/src/services/http.servise.js
@@ -3,6 +3,14 @@ import endpoint from '../config'
 
 const http = axios.create({ baseURL: endpoint })
 
+http.interceptors.request.use(async (config) => {
+	config.headers = {
+		...config.headers,
+		'Access-Control-Allow-Origin': '*',
+		'Access-Control-Allow-Metods': '*',
+	}
+})
+
 const httpService = {
 	get: http.get,
 	post: http.post,
